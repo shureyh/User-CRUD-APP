2390f3f465243fa25726baaec32632d57360e9fc
shureyh <116615940+shureyh@users.noreply.github.com>
2022-11-23 10:42:08 +0300

    set up routes, models and schemas for DB and app.js entry point

---
+++ app.js
@@ -0,0 +1,99 @@
+//imports
+
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//database connection
+mongoose.connect(process.env.MONGO_DEV_URI, {
+
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("Database connected!")).catch(err => console.log(err));
+
+
+//middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(session({
+  secret: "my secret key",
+  saveUninitialized: true,
+  resave: false,
+})
+);
+app.use((req, res, next) =>{
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
+
+//set template engine
+app.set("view engine", "ejs");
+
+
+// route prefix
+app.use("", require("./routes/routes"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function () {
+  console.log("Server Started at http://localhost:" + PORT);
+});
